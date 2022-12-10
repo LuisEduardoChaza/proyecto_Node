9fa45388442f99c827638ee881b5ddc1fab7e7f4
LuisEduardoChaza <chavezluiseduardo3@gmail.com>
2022-12-10 16:49:50 -0600

    update

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const {empleado} = require('./empleados.json');
+
+app.get("/", (req, res, next) => {
+    res.send(empleado);
+});
 
 app.listen(process.env.PORT || 4000, () => {
     console.log('Server is running...');
